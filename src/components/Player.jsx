42ef30562f1832418cc0635057b18f5adecd7ece
tsheporamantso <tshepo.ramantso@outlook.com>
2024-04-30 11:21:00 +0200

    Add useState hook and conditionally render player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 export default function Player() {
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  
+  const handleChange = (e) => {
+    setEnteredPlayerName(e.target.value);
+  };
+
+  const handleClick = () => { 
+    setSubmitted(true)
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={handleChange} value={enteredPlayerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
